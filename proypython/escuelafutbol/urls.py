e0f88056cf66ac011991cadff45e038262897cd5
Walter <solucionesweps@gmail.com>
2023-07-03 20:06:04 +0200

    Actualizacion

--- proypython/escuelafutbol/urls.py
+++ proypython/escuelafutbol/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router=DefaultRouter()
+router.register(r"categorias", views.CategoriaViewSet)
 
 urlpatterns=[
-    path("",views.index, name="index"),
+    #path("",views.index, name="index"),
+    # path("categorias/", views.categorias,name="categorias"),
+    path("",include(router.urls)),
 ]
--- proypython/escuelafutbol/views.py
+++ proypython/escuelafutbol/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-#Libreria que permite mostrar respuestas en formato http
 from django.http import HttpResponse
+from .models import Categoria
+#Libreria que permite mostrar respuestas en formato http
+from rest_framework import viewsets
+from .serializers import CategoriaSerializer
 
 def index(request):
     return HttpResponse("Hola Escuela de Futbol...")
 
-# Create your views here.
+def categorias(request):
+    post_nombre=request.POST.get("nombre")
+    if post_nombre:
+        categoria=Categoria(nombre_categoria=post_nombre)
+        categoria.save()
+    categorias=Categoria.objects.all()
+    return render(request,"form_categorias.html",{"categorias": categorias})
+
+class CategoriaViewSet(viewsets.ModelViewSet):
+    queryset=Categoria.objects.all()
+    serializer_class=CategoriaSerializer
